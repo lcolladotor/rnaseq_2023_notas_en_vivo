1fddf7cbb118dda7a91ee09b923b05c8ba66a022
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-01-31 10:53:15 -0600

    Agregué un ¡

--- R/02-visualizar-mtcars.R
+++ R/02-visualizar-mtcars.R
@@ -3,7 +3,7 @@ library("here")
 library("ggplot2")
 
 ## Hello world
-print("Soy Leo (en vivo desde Cuerna!)")
+print("Soy Leo (¡en vivo desde Cuerna!)")
 
 ## Directorios
 dir_plots <- here::here("figuras")
