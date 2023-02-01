6978a3dc807b94e78f4c4a0316a7bc00e518a0ef
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-02-01 14:15:57 -0600

    Simplificar el script de app.R

--- app.R
+++ app.R
@@ -15,11 +15,7 @@ library("recount3")
 
 
 ## URL de recount3
-getOption("recount3_url",
-    "http://duffel.rail.bio/recount3")
 options(recount3_url = "https://recount-opendata.s3.amazonaws.com/recount3/release")
-getOption("recount3_url",
-    "http://duffel.rail.bio/recount3")
 
 ## ----'quick_example'--------------------------------
 ## Revisemos todos los proyectos con datos de humano en recount3
@@ -41,20 +37,6 @@ proj_info <- structure(list(project = "SRP009615", organism = "human", file_sour
 ## Crea un objeto de tipo RangedSummarizedExperiment (RSE)
 ## con la información a nivel de genes
 rse_gene_SRP009615 <- create_rse(proj_info)
-## Explora el objeto RSE
-rse_gene_SRP009615
-
-
-## ----"interactive_display", eval = FALSE------------
-## ## Explora los proyectos disponibles de forma interactiva
-## proj_info_interactive <- interactiveDisplayBase::display(human_projects)
-## ## Selecciona un solo renglón en la tabla y da click en "send".
-##
-## ## Aquí verificamos que solo seleccionaste un solo renglón.
-## stopifnot(nrow(proj_info_interactive) == 1)
-## ## Crea el objeto RSE
-## rse_gene_interactive <- create_rse(proj_info_interactive)
-
 
 ## ----"tranform_counts"------------------------------
 ## Convirtamos las cuentas por nucleotido a cuentas por lectura
@@ -67,9 +49,6 @@ assay(rse_gene_SRP009615, "counts") <- compute_read_counts(rse_gene_SRP009615)
 ## Para este estudio en específico, hagamos más fácil de usar la
 ## información del experimento
 rse_gene_SRP009615 <- expand_sra_attributes(rse_gene_SRP009615)
-colData(rse_gene_SRP009615)[
-    ,
-    grepl("^sra_attribute", colnames(colData(rse_gene_SRP009615)))
-]
 
+## Crear el sitio web interactivo
 iSEE::iSEE(rse_gene_SRP009615)
